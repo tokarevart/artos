ab19b1af190abf403e12b8e261bd4cf1593f164e
Tokarev Artem <tokarev28.art@gmail.com>
2025-10-06 04:01:38 +0500

    move MutexPtr to mutex module

--- src/sync.rs
+++ src/sync.rs
@@ -1,9 +1,8 @@
 #![allow(unused_imports)]
 
 pub mod mutex;
-pub mod mutex_ptr;
 
 pub use mutex::Mutex;
 pub use mutex::MutexGuard;
-pub use mutex_ptr::MutexPtr;
-pub use mutex_ptr::MutexPtrGuard;
+pub use mutex::MutexPtr;
+pub use mutex::MutexPtrGuard;
--- src/sync/mutex.rs
+++ src/sync/mutex.rs
@@ -1,6 +1,7 @@
 use core::cell::UnsafeCell;
 use core::ops::Deref;
 use core::ops::DerefMut;
+use core::ptr::NonNull;
 use core::sync::atomic::AtomicBool;
 use core::sync::atomic::Ordering;
 
@@ -65,5 +66,48 @@ impl<T: ?Sized> Drop for MutexGuard<'_, T> {
     #[inline]
     fn drop(&mut self) {
         self.mutex.is_locked.store(false, Ordering::Release);
+    }
+}
+
+#[derive(Debug)]
+pub struct MutexPtr<T: ?Sized>(Mutex<NonNull<T>>);
+
+impl<T> MutexPtr<T> {
+    pub const unsafe fn new(t: NonNull<T>) -> Self {
+        Self(Mutex::new(t))
+    }
+}
+
+impl<T: ?Sized> MutexPtr<T> {
+    /// Non-blocking attempt to acquire the lock.
+    /// Returns `Some(MutexGuard)` on success, `None` on failure.
+    #[inline]
+    pub fn try_lock(&self) -> Option<MutexPtrGuard<'_, T>> {
+        self.0.try_lock().map(MutexPtrGuard)
+    }
+}
+
+unsafe impl<T: ?Sized + Send> Send for MutexPtr<T> {}
+unsafe impl<T: ?Sized + Send> Sync for MutexPtr<T> {}
+
+#[derive(Debug)]
+pub struct MutexPtrGuard<'a, T: ?Sized>(MutexGuard<'a, NonNull<T>>);
+
+unsafe impl<T: ?Sized + Sync> Send for MutexPtrGuard<'_, T> {}
+unsafe impl<T: ?Sized + Sync> Sync for MutexPtrGuard<'_, T> {}
+
+impl<T: ?Sized> Deref for MutexPtrGuard<'_, T> {
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.0.deref().as_ptr() }
+    }
+}
+
+impl<T: ?Sized> DerefMut for MutexPtrGuard<'_, T> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.0.deref().as_ptr() }
     }
 }
